40c0126fc27c9ae12175ed352efb0ea71c37ea04
Krzysztof Wozniczak <kwoznicz@volvocars.com>
2025-01-21 06:08:01 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ExampleWpfApp.Velopack.GoogleDrive
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
